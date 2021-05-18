58264eb8bc9bd3f7d7da6ef0057a9c704c9bf033
Zephyro Kemstedt <zkemstedt@gmail.com>
2021-05-18 17:27:38 +0200

    cleanup sheets.js use resources/config.js use resources/creds.js sheetsByTitle -> sheetsByID remove old members fix misc cleanup

--- modules/sheets.js
+++ modules/sheets.js
@@ -1,71 +1,76 @@
 const { GoogleSpreadsheet } = require('google-spreadsheet');
-const Logger = require('../classes/rerelog');
-const log = new Logger(5);
-const creds = require('/Users/Zephyro/Downloads/quickstart-1599814377442-5ec02b6819bb.json');
 
-const spreadsheetid = '1vG4wBLovRAaLqimX4twHhwhhnFTTS4aUcFrRVatxiS0';
-const sheetname = '[DEV] Remtest';
+const mode = process.env.TOKEN ? 'prod' : 'dev';
+const config = require('../resources/config.json')[mode];
+const creds = require('../resources/creds.json');
+const Logger = require('../classes/rerelog');
+
+const log = new Logger(config.loglevel);
+const spreadsheetid = config.NWSpreadsheetID;
+const sheetid = config.NWSheetID;
+
 
 /**
+ * Upload data to google sheets.
  * 
- * 
+ * @param {{id, name, class, ap, aap, dp, gs, succession, axe, horse}[]}
+ * actually the paramter is missing status and the days, but line too long anyway...
  */
 async function run (data) {
-    
-    // Initialize the sheet - doc ID is the long id in the sheets URL
-    let doc = new GoogleSpreadsheet(spreadsheetid);
-
-    await doc.useServiceAccountAuth(creds);
-
-    await doc.loadInfo(); // loads document properties and worksheets
     log.info('Google Sheet sync START');
+
+    let doc = new GoogleSpreadsheet(spreadsheetid);
+    await doc.useServiceAccountAuth(creds);
+    await doc.loadInfo();
+
     log.info(`Target sheet -> ${doc.title}`);
 
-    const sheet = doc.sheetsByTitle[sheetname];
+    const sheet = doc.sheetsById[sheetid];
     let rows = await sheet.getRows({'limit': 101}); // 100 + header
     
     let i;
-    const max = data.length > rows.length ? data.length : rows.length;
-    log.debug(`data length: ${data.length}`);
-    log.debug(`rows length: ${rows.length}`);
-    log.trace(`iteration count: ${max}`);
+    let rowcount = data.length > rows.length ? data.length : rows.length;
+    log.trace(`data length: ${data.length}`);
+    log.trace(`row length: ${rows.length}`);
+    log.trace(`doing ${rowcount} iterations...`);
     
-    for (i = 0; i < max; i++) {
+    for (i = 0; i < rowcount; i++) {
         log.trace(`i:${i}`);
         // both are defined => overwrite
         if (rows[i] !== undefined && data[i] !== undefined) {
             log.trace(`data: ${data[i].name}`);
             log.trace(`row: ${rows[i].name}`)
-            rows[i].name = data[i].name;
             Object.keys(data[i]).map((key, j) => {
-                // log(`> TRACE: i: ${i}`);
-                log.trace(`key: ${key}, value: ${data[i][key]}`);
+                log.trace(`   ${key}: ${data[i][key]}`);
                 rows[i][key] = data[i][key];
             })
             log.trace('saving...');
             await rows[i].save();
             log.trace('Done');
+            log.trace('================');
         }
-        // sheet > members => delete
+        // sheet > members => the remaining entries should be deleted.
         else if (rows[i] !== undefined) {
-            log.debug(`Old member removed from nw sheet: ${JSON.stringify(rows[i].name)}`)
-            await rows[i].delete(); // VERIFY will this impact the length or rows?
+            // deleting a row will update gsheets but not our rows variable
+            // meaning that each subsequent local index will point to a decremented 
+            // remote index.
+            // delete (1), delete (2), delete (3) will delete i=1, i=3 and i=5
+
+            // to get around this we will loop through the rest of the rows in reverse order.
+            for (let j = rowcount - 1; j >= i; j--) {
+                log.debug(`Old member removed from nw sheet: ${JSON.stringify(rows[j].name)}`)
+                await rows[j].delete();
+            }
+            // stop the outer loop since we deleted the remaining entries.
+            break;
         }
         // members > sheet => add new row
         else if (data[i] !== undefined) {
-            let t1 = await sheet.addRow(data[i]); // VERIFY
-            log.debug(`New member added to nw sheet: ${JSON.stringify(t1)}`);
+            let newrow = await sheet.addRow(data[i]); // VERIFY
+            log.debug(`New member added to nw sheet: ${JSON.stringify(newrow.name)}`);
         }
-        // SHOULD be unreachable
-        else {
-            log.warn("Unreachable branch-condition in ./modules/sheets.js - run)");
-        }
-        log.trace('================');
     }
     log.info('Google Sheet sync END');
 };
 
-
 module.exports.doSheetUpload = run;
-// rem-5-892@quickstart-1599814377442.iam.gserviceaccount.com
-// 118232451383662433091
