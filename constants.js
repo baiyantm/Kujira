a3e0f7d709a26449353576a3282c3d04ae58a597
Zephyro <zkemstedt@gmail.com>
2021-05-25 13:41:57 +0200

    merge logging configs into constants.
    Expanding constants to eventually replace config.json and items.json as well

--- constants.js
+++ constants.js
@@ -1,5 +1,22 @@
-const constants = process.env.TOKEN ?
+const c = process.env.TOKEN ?
     require('./resources/static-default.json') :
     require('./resources/static.json');
 
-module.exports = constants
+c.Guilds.Remedy.BlacklistedMemberChannels = [
+    c.Guilds.Remedy.attendance_updates,
+    c.Guilds.Remedy.guild_wars,
+    c.Guilds.Remedy.useful_stuff
+];
+
+c.Guilds.Remedy.WhitelistedMemberChannels = [
+    c.Guilds.Remedy.guild_chat,
+    c.Guilds.Remedy.nodewar_pvp,
+    c.Guilds.Remedy.bots
+];
+
+// Allow lookup by name or id
+c[c.Guilds.Remedy.id] = c.Guilds.Remedy;
+
+c["prefix"] = "?";
+
+module.exports = c;
