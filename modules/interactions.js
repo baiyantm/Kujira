44df073b10559210522ccfe31b4ba9c40a949445
Zephyro <zkemstedt@gmail.com>
2021-06-02 10:14:09 +0200

    cleanup docstrings and add some typechecks + logs

--- modules/interactions.js
+++ modules/interactions.js
@@ -1,6 +1,16 @@
 const log = require('log4js').getLogger('modules/interactions');
 
-const Discord = require('discord.js');
+const {
+    User,
+    GuildMember,
+    Message,
+    PartialMessage,
+    MessageEmbed,
+    GuildChannel,
+    TextChannel,
+    DMChannel,
+    NewsChannel
+} = require('discord.js');
 
 const { startLoading, endLoading } = require('../utils/messages');
 
@@ -8,6 +18,7 @@ const { startLoading, endLoading } = require('../utils/messages');
 
 /**
  * Wrapper for start/end loading
+ * 
  * @param {Function} fn 
  * @returns 
  */
@@ -18,22 +29,27 @@ async function wrapLoading(message, fn, ...args) {
 }
 
 /**
- * wrapper to edit a bot message
- * @param {Discord.Message} message the discord message linked
- * @param {string | Discord.MessageEmbed} content the content to send
- * @returns whether the message got edited
+ * Wrapper to safely edit a bot message
+ * 
+ * @param {Message} message the discord message linked
+ * @param {String | MessageEmbed} content the content to send
+ * @returns {?Boolean} whether the message got edited
  */
 async function wEditMsg(message, content) {
     var edit = true;
+    if (!message instanceof Message) {
+        log.error(`wEdit expected a Message, but got ${typeof message}: ${message}`);
+        return false;
+    }
     try {
         await message.edit(content);
         let name = message.channel.toString();
-        if(message.channel instanceof Discord.GuildChannel) {
+        if(message.channel instanceof GuildChannel) {
             name = message.channel.name;
         }
         log.info('EDIT: ' + content + " in " + name);
     } catch (e) {
-        log.error(`Couldn't edit the message ${e}`);
+        log.error(`Couldn't edit the message ${message.id} ${e}`);
         edit = false;
     }
     return edit;
@@ -41,58 +57,72 @@ async function wEditMsg(message, content) {
 
 
 /**
- * wrapper to send a bot message to a channel
- * @param {Discord.TextChannel | Discord.DMChannel | Discord.NewsChannel} channel The discord channel
- * @param {Discord.APIMessage | Discord.StringResolvable} content The content to send
- * @returns {?Discord.Message}the message sent
+ * wrapper to safely send a bot message to a channel
+ * 
+ * @param {TextChannel | DMChannel | NewsChannel} channel The discord channel
+ * @param {String | MessageEmbed} content The content to send
+ * @returns {?Message} The message sent
  */
 async function wSendChannel(channel, content) {
     var sent;
-    try {
-        sent = await channel.send(content);
-        let name = channel.toString();
-        if(channel instanceof Discord.GuildChannel) {
-            name = channel.name;
+    if (!(channel instanceof TextChannel || channel instanceof NewsChannel || channel instanceof DMChannel)) {
+        log.error(`wSendChannel expected a Channel, but got ${typeof channel}`)
+    } else {
+        if (channel instanceof DMChannel) {
+            log.warn(`Using wSendChannel for DMs is discouraged, use wSendAuthor`);
         }
-        logger.log("SENT: `" + content + "` in " + name);
-    } catch (e) {
-        console.error(e);
-        log.info("Couldn't send a message in " + channel);
+        try {
+            sent = await channel.send(content);
+            let name = channel.toString();
+            if(channel instanceof GuildChannel) {
+                name = channel.name;
+            }
+            logger.log("SENT: `" + content + "` in " + name);
+        } catch (e) {
+            console.error(e);
+            log.info("Couldn't send a message in " + channel);
+        }
     }
     return sent;
 }
 
 /**
- * Wrapper to send a bot message to the author
- * @param {Discord.User} author the discord author
- * @param {string | Discord.MessageEmbed} content the content to send
- * @returns the message sent
+ * Wrapper to safely send a bot message to the author
+ * 
+ * @param {User | GuildMember} author The discord User or Member to dm
+ * @param {String | MessageEmbed} content The content to send
+ * @returns {?Message} The message sent
  */
 async function wSendAuthor(author, content) {
     var sent;
-    try {
-        sent = await author.send(content);
-        log.info("SENT: DM to [" + author.tag + " - " + author.id + "]");
-    } catch (e) {
-        log.error("Couldn't send a message to [" + author.tag + " - " + author.id + `]\n${e}`);
+    if (!(author instanceof User || author instanceof GuildMember)) {
+        log.error(`wSendAuthor expected GuildMember|User, but instead got ${typeof author}`);
+    } else {
+        try {
+            sent = await author.send(content);
+            log.info("SENT: DM to [" + author.tag + " - " + author.id + "]");
+        } catch (e) {
+            log.error("Couldn't send a message to [" + author.tag + " - " + author.id + `]\n${e}`);
+        }
     }
     return sent;
 }
 
 /**
- * Wrapper to delete a message
- * @param {Discord.Message | Discord.PartialMessage} message the message to delete
+ * Wrapper to safely delete a message
+ * 
+ * @param {Message | PartialMessage} message the message to delete
  */
 async function wDelete(message, timeout=0) {
     try {
         await message.delete({timeout: timeout});
         let name = message.channel.toString();
-        if(message.channel instanceof Discord.GuildChannel) {
+        if(message.channel instanceof GuildChannel) {
             name = message.channel.name;
         }
         log.info("Deleted `" + message.content + "` from " + name);
     } catch (e) {
-        log.error(`Tried to delete an already deleted message or not enough permissions. \n${e}`);
+        log.error(`Failed to delete message!\n${e}`);
     }
 }
 
